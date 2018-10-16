648d9501923aa1c9719b237cf29d987ca539f144
IkennaOkafor <ikennajeshuaokafor@gmail.com>
2018-10-16 00:32:17 -0400

    Updates

--- inc/RL.hpp
+++ inc/RL.hpp
@@ -10,24 +10,29 @@
 #include <vector>
 #include <ctime>
 #include <iostream>
+#include <tuple>
+#include <functional>
 
 
 #include "util.hpp"
 
+
 class QLearner {
     public:
-	QLearner(int numStates, int numActionsPerState, bool *validActionsPerState, int *transitionMatrix, float discountFactor = 0.9f, int startState = -1, float epsilon = 0.2f);
+	QLearner(int numStates, int numActionsPerState, bool *validActionsPerState, int *transitionMatrix, float epsilon = 0.2f, float epsilonDecayFactor = 0.99f, float discountFactor = 0.9f,  float learningRate = 1.0f, int startState = -1);
 		~QLearner();
 		int GetNextAction();
 		int GetBestAction(int state);
 		void UpdateQTable(float reward);
-		void printQMatrix();
+		void NextState();
+		void PrintQMatrix();
+		
         
         float *m_QMatrix;
         float m_discountFactor;
 		float m_learningRate;
 		float m_epsilon;
-		float m_epsilonDecay;
+	    float m_epsilonDecayFactor;
 		int m_currentState;
 		int m_numStates;
 		int m_numActionsPerState;
@@ -36,6 +41,7 @@ class QLearner {
 		int *m_transitionMatrix;
 		std::default_random_engine *m_gen;
 		std::uniform_real_distribution<float> *m_dis;
+	
 	
     protected:
     
--- src/RL.cpp
+++ src/RL.cpp
@@ -1,30 +1,28 @@
 #include "RL.hpp"
 using namespace std;
 
-QLearner::QLearner(int numStates, int numActionsPerState, bool *validActionsPerState, int *transitionMatrix, float discountFactor, int startState, float epsilon) {
+
+QLearner::QLearner(int numStates, int numActionsPerState, bool *validActionsPerState, int *transitionMatrix, float epsilon, float epsilonDecayFactor, float discountFactor, float learningRate, int startState) {
+	srand(time(NULL));
 	m_numStates = numStates;
 	m_numActionsPerState = numActionsPerState;
 	m_validActionsPerState = (bool*)malloc(numStates * numActionsPerState * sizeof(bool));
 	memcpy(m_validActionsPerState, validActionsPerState, (numStates * numActionsPerState * sizeof(bool)));
 	m_discountFactor = discountFactor;
 	m_epsilon = epsilon;
+	m_epsilonDecayFactor = epsilonDecayFactor;
+	m_learningRate = learningRate;
 	m_QMatrix = (float*)malloc(numStates * numActionsPerState * sizeof(float));
 	for (int i = 0; i < (numStates * numActionsPerState); i++){
-		m_QMatrix[i] = 1000.0f;
-	}
-    cout << "......Initial Q-Matrix......" << endl;
-	printQMatrix();
-	
+		m_QMatrix[i] = 0.0f;
+	}	
 	m_transitionMatrix = (int*)malloc(numStates * numActionsPerState * sizeof(int));
-	memcpy(m_transitionMatrix, transitionMatrix, (numStates * numActionsPerState * sizeof(int)));
-	
+	memcpy(m_transitionMatrix, transitionMatrix, (numStates * numActionsPerState * sizeof(int)));	
 	if(startState == -1) {
 		m_currentState = 0;
 	} else {
 		m_currentState = startState;
-	}
-	
-
+	}	
 	m_gen = new default_random_engine();
 	m_dis = new uniform_real_distribution<float>(0.0f, 1.0f);
 }
@@ -33,22 +31,25 @@ QLearner::QLearner(int numStates, int numActionsPerState, bool *validActionsPerS
 QLearner::~QLearner() {
     free(m_QMatrix);
 	free(m_validActionsPerState);
+	free(m_transitionMatrix);
+	delete m_gen;
+	delete m_dis;
 }
 
 
 int QLearner::GetNextAction() {
 	if((m_dis[0](m_gen[0])) <= m_epsilon) {
-		srand(time(NULL));
 		while(true) {
 			m_currentAction = rand() % m_numActionsPerState;
 			if (index2D(m_numStates, m_numActionsPerState, m_validActionsPerState, m_currentState, m_currentAction)) {
-				//m_epsilon -= 0.001f;
+				m_epsilon -= m_epsilonDecayFactor;
+				cout << "[RL_LIB] Agent chose random action. Epsilon is" << " " << m_epsilon << "." << endl;					
 				return m_currentAction;
 			}
 		}
 	} else {
 		m_currentAction = GetBestAction(m_currentState);
-		//m_epsilon -= 0.001f;
+		cout << "[RL_LIB] Agent chose best action." << endl;
 		return m_currentAction;
 	}
 }
@@ -57,6 +58,7 @@ int QLearner::GetNextAction() {
 int QLearner::GetBestAction(int state) {
 	vector<int> actionList;
 	int bestAction;
+	
 	for (int action = 0; action < m_numActionsPerState; action++) {
 		if (index2D(m_numStates, m_numActionsPerState, m_validActionsPerState, state, action)) {
 			if (actionList.size() == 0) {
@@ -74,7 +76,6 @@ int QLearner::GetBestAction(int state) {
 		bestAction = actionList[0];
 		return bestAction;
 	} else {
-		srand(time(NULL));
 		bestAction = actionList[rand() % actionList.size()];
 		return bestAction;
 	}
@@ -84,15 +85,26 @@ int QLearner::GetBestAction(int state) {
 void QLearner::UpdateQTable(float reward) {
 	int nextState = index2D(m_numStates, m_numActionsPerState, m_transitionMatrix, m_currentState, m_currentAction);
 	int bestAction = GetBestAction(nextState);
-	index2D(m_numStates, m_numActionsPerState, m_QMatrix, m_currentState, m_currentAction)
-		= (reward + m_discountFactor * index2D(m_numState, m_numActionsPerState, m_QMatrix, nextState, bestAction));
-	m_currentState = nextState;
+	index2D(m_numStates, m_numActionsPerState, m_QMatrix, m_currentState, m_currentAction) = 
+		(1.0f - m_learningRate) * index2D(m_numStates, m_numActionsPerState, m_QMatrix, m_currentState, m_currentAction) + m_learningRate
+		* (reward + m_discountFactor * index2D(m_numState, m_numActionsPerState, m_QMatrix, nextState, bestAction));
 }
 
-void QLearner::printQMatrix() {
+
+void QLearner::NextState() {
+	m_currentState = index2D(m_numStates, m_numActionsPerState, m_transitionMatrix, m_currentState, m_currentAction);
+}
+
+
+void QLearner::PrintQMatrix() {
 	for (int i = 0; i < m_numStates; i++) {
+		cout << "state_" << i << " ";
 		for (int j = 0; j < m_numActionsPerState; j++) {
-			cout << index2D(agent->m_numStates, m_numActionsPerState, m_QMatrix, i, j) << " ";
+			if (index2D(m_numStates, m_numActionsPerState, m_validActionsPerState, i, j)) {
+				cout << index2D(m_numStates, m_numActionsPerState, m_QMatrix, i, j) << " ";
+			} else {
+				cout << -1 << " ";
+			}
 		}
 		cout << endl;
 	}
